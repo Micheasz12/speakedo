951b85c42394d0473fbef42fa5ae3e2a7c812649
Micheasz12 <pilsniakmicheasz@gmail.com>
2024-09-01 12:46:05 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+// Przykładowy kod JavaScript dla dodatkowej funkcjonalności
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Dziękujemy za zapisanie się na lekcję próbną!");
+});
 
