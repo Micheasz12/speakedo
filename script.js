36c90cadfb3923238c9c53e8f5588088d4522046
Micheasz12 <pilsniakmicheasz@gmail.com>
2024-09-02 18:03:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
-document.querySelector("form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Dziękujemy za zapisanie się na lekcję próbną!");
-});
-
 document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector("form");
+    if (form) {
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            alert("Dziękujemy za zapisanie się na lekcję próbną!");
+        });
+    }
+
     const prevBtn = document.querySelector('.prev');
     const nextBtn = document.querySelector('.next');
     const testimonialContainer = document.querySelector('.testimonial-container');
     const testimonials = document.querySelectorAll('.testimonial');
-    let currentIndex = 0;
 
-    function updateSlider() {
-        testimonialContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    if (prevBtn && nextBtn && testimonialContainer && testimonials.length > 0) {
+        let currentIndex = 0;
+
+        function updateSlider() {
+            testimonialContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+        }
+
+        prevBtn.addEventListener('click', function () {
+            if (currentIndex > 0) {
+                currentIndex--;
+            } else {
+                currentIndex = testimonials.length - 1; // Ostatnia opinia
+            }
+            updateSlider();
+        });
+
+        nextBtn.addEventListener('click', function () {
+            if (currentIndex < testimonials.length - 1) {
+                currentIndex++;
+            } else {
+                currentIndex = 0; // Powrót do pierwszej opinii
+            }
+            updateSlider();
+        });
     }
-
-    prevBtn.addEventListener('click', function () {
-        if (currentIndex > 0) {
-            currentIndex--;
-        } else {
-            currentIndex = testimonials.length - 1; // Ostatnia opinia
-        }
-        updateSlider();
-    });
-
-    nextBtn.addEventListener('click', function () {
-        if (currentIndex < testimonials.length - 1) {
-            currentIndex++;
-        } else {
-            currentIndex = 0; // Powrót do pierwszej opinii
-        }
-        updateSlider();
-    });
 });
