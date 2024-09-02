d92edfdc62b948332760bf472f6139e6ec8f2665
Micheasz12 <pilsniakmicheasz@gmail.com>
2024-09-02 17:23:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ document.querySelector("form").addEventListener("submit", function(event) {
     alert("Dziękujemy za zapisanie się na lekcję próbną!");
 });
 
+document.addEventListener('DOMContentLoaded', function () {
+    const prevBtn = document.querySelector('.prev');
+    const nextBtn = document.querySelector('.next');
+    const testimonialContainer = document.querySelector('.testimonial-container');
+    let currentIndex = 0;
+
+    prevBtn.addEventListener('click', function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+        } else {
+            currentIndex = testimonialContainer.children.length - 1;
+        }
+        updateSlider();
+    });
+
+    nextBtn.addEventListener('click', function () {
+        if (currentIndex < testimonialContainer.children.length - 1) {
+            currentIndex++;
+        } else {
+            currentIndex = 0;
+        }
+        updateSlider();
+    });
+
+    function updateSlider() {
+        testimonialContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+});
