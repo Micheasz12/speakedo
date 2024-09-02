33b238dd09887ffed41cc0a2ee694b8be277e669
Micheasz12 <pilsniakmicheasz@gmail.com>
2024-09-02 17:28:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,27 +9,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const prevBtn = document.querySelector('.prev');
     const nextBtn = document.querySelector('.next');
     const testimonialContainer = document.querySelector('.testimonial-container');
+    const testimonials = document.querySelectorAll('.testimonial');
     let currentIndex = 0;
+
+    function updateSlider() {
+        testimonialContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
 
     prevBtn.addEventListener('click', function () {
         if (currentIndex > 0) {
             currentIndex--;
         } else {
-            currentIndex = testimonialContainer.children.length - 1;
+            currentIndex = testimonials.length - 1;
         }
         updateSlider();
     });
 
     nextBtn.addEventListener('click', function () {
-        if (currentIndex < testimonialContainer.children.length - 1) {
+        if (currentIndex < testimonials.length - 1) {
             currentIndex++;
         } else {
             currentIndex = 0;
         }
         updateSlider();
     });
-
-    function updateSlider() {
-        testimonialContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
 });
